38f7aec9b66c8dcd8eca55435d8c41faa5c4a7cc
Jingyi Wu <jingywu@seas.upenn.edu>
2021-09-23 22:20:38 -0400

    fix bug

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,17 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="outerBox">
       <div className="box">
-      <img className="img" src={logo} alt="logo"></img>
-      <img className="img" src={logo} alt="logo"></img>
-      <img className="img" src={logo} alt="logo"></img>
-      <div className="innerBox">The Beagle is called Bella.</div>
-      <div className="innerBox">The Siamese cat is called Louis.</div>
-      <div className="innerBox">The Abyssinian cat is called Leon.</div>
-   </div>
+        <img className="img" src={logo} alt="logo"></img>
+        <img className="img" src={logo} alt="logo"></img>
+        <img className="img" src={logo} alt="logo"></img>
+        <div className="innerBox">The Beagle is called Bella.</div>
+        <div className="innerBox">The Siamese cat is called Louis.</div>
+        <div className="innerBox">The Abyssinian cat is called Leon.</div>
+      </div>
     </div>
   );
 }
